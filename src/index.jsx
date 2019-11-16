0086ace28d38f850a602e3c6a05023ce252e7f7d
Michael Flinn <mflinn92@gmail.com>
2019-11-16 14:24:13 -0500

    Confirmed basic babel/webpack config is working Able to load and serve a static page which renders a react component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.log('basic webpack config test');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="reactTest">
+      React Test
+    </div>
+  );
+}
+
+ReactDom.render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options : {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
