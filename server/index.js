fab41bc2d52cc9dbe3d7e6883a0cd0e51dc2ed5c
Michael Flinn <mflinn92@gmail.com>
2019-11-16 14:06:55 -0500

    Confirmed basic webpack configs function no react yet, just confirmed bundle.js is created

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 const PORT = 1234;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server up');
-})
+app.use(morgan('dev'));
+app.use(express.static('./dist'));
+
+// app.get('/', (req, res) => {
+//   res.send('Server up');
+// })
 
 app.listen(PORT, () => {
   console.log('listening at port ' + PORT);
-})
+});
